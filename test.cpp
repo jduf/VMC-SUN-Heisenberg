f21207698f1ca9463915fecc00476486e04c5081
Jérôme Dufour <jerome.dufour@epfl.ch>
2013-01-10 15:58:41 +0100

    certainement avant fusion master use-system

--- test.cpp
+++ test.cpp
@@ -7,25 +7,28 @@ double energie(unsigned int N_spin, unsigned int N_m, arma::Mat<double> *U);
 int main(){
 	Chrono t;
 	t.tic();
-	unsigned int const N_spin(3);
-	unsigned int const N_m(6);
-	unsigned int const N_site(N_spin*N_m);
+	for(unsigned int i(0);i<10;i++){
+		unsigned int const N_spin(3);
+		unsigned int const N_m(6);
+		unsigned int const N_site(N_spin*N_m);
 
-	arma::Mat<double> U(arma::zeros(N_site,N_site));
-	U(0,1)=-1.0;
-	U(0,N_site-1)=1.0;
-	for(unsigned int i(1); i< N_site-1; i++){
-		U(i,i-1) = -1.0;
-		U(i,i+1) = -1.0;
+		arma::Mat<double> U(arma::zeros(N_site,N_site));
+		U(0,1)=-1.0;
+		U(0,N_site-1)=1.0;
+		for(unsigned int i(1); i< N_site-1; i++){
+			U(i,i-1) = -1.0;
+			U(i,i+1) = -1.0;
+		}
+		U(N_site-1,0)=1.0;
+		U(N_site-1,N_site-2)=-1.0;
+
+		arma::Col<double> EVal;
+		arma::Mat<double> EVec;
+		arma::eig_sym(EVal,EVec,U);
+
+		std::cout<<energie(N_spin,N_m,&EVec);
 	}
-	U(N_site-1,0)=1.0;
-	U(N_site-1,N_site-2)=-1.0;
-	
-	arma::Col<double> EVal;
-	arma::Mat<double> EVec;
-	arma::eig_sym(EVal,EVec,U);
-	
-	std::cout<<energie(N_spin,N_m,&EVec);
+
 	t.tac();
 	std::cout<<" en "<<t<<std::endl;
 }
@@ -36,7 +39,7 @@ double energie(unsigned int N_spin, unsigned int N_m, arma::Mat<double> *U){
 	State beta(N_site);
 	State tmp(N_site);
 	double ratio(0.0), energie(0.0);
-	unsigned int i(0),NMC(1e3);
+	unsigned int i(0),NMC(1e5);
 	while(i<NMC){
 		tmp = alpha.swap();
 		ratio = (tmp.Det() * tmp.Det()) / (alpha.Det() * alpha.Det());
