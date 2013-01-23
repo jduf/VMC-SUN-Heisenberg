f6702fe6ff93401edce019b994f9ae9d43a6f349
Jérôme Dufour <jerome.dufour@epfl.ch>
2013-01-23 14:49:23 +0100

    6% de différence entre master et my-matrice

--- test.cpp
+++ test.cpp
@@ -18,19 +18,17 @@ int main(){
 
 double energie(System *S){
 	State alpha(S);
-	State beta(S->N_site);
-	State tmp(S->N_site);
 	double ratio(0.0), energie(0.0);
 	unsigned int i(0),NMC(1e4);
 	while(i<NMC){
-		tmp = alpha.swap();
+		State tmp(alpha.swap());
 		ratio = (tmp.Det() * tmp.Det()) / (alpha.Det() * alpha.Det());
 		if(ratio>1 || double(rand())/RAND_MAX <ratio ){
 			i++;
 			alpha = tmp;
 			for(unsigned int j(0);j<S->N_site;j++){
 				for(unsigned int d(0);d<S->dim;d++){
-					beta = alpha.swap(j,S->nts[S->dim*j+d]);
+					State beta(alpha.swap(j,S->nts[S->dim*j+d]));
 					energie += beta.Det()/alpha.Det();
 				}
 			}
